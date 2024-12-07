d52c851c5d6b9b3470d6d8a9e1d9992bfe12c01d
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-07 10:28:06 +0530

    Add files via upload

---
+++ HallowRevTriangle1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class HallowRevTriangle1
+/*
+* * * * * * 
+  *       *
+    *     *
+      *   *
+        * *
+          *
+ */
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new  Scanner(System.in);
+		int n=sc.nextInt();
+        for(int i=0; i<n; i++)
+         {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0||i==j||j==n-1)
+                System.out.print("*"+" ");
+                else
+                System.out.print(" "+" ");
+            }
+         
+            
+        System.out.println();
+        
+        
+        }
+	}
+}
