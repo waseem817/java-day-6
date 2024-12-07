91e2b86dec0a5709d4c815786df8665877108b21
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-07 10:45:00 +0530

    Add files via upload

---
+++ PatternButterfly.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+class PatternButterfly
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new  Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+	    {
+		    for(int j=0;j<n;j++)
+		    {
+			   if(j==0 || i==j || i+j==n-1 || j==n-1 )
+			   System.out.print("*"+" ");
+			   else
+			   System.out.print(" "+" ");
+		    }
+		    System.out.println();
+        }
+    }
+}            
+   
