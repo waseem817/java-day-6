1be7244a6d286517f6a5f8359f5d6d6b0a50c97d
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-07 12:38:24 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+class Array
+{
+	public static void main (String[] args) 
+	{
+        //initialization of array
+        int arr[]={22,44,565,789,200};
+        System.out.println(Arrays.toString(arr));
+        //Accessing of array
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        //Modification of array
+        arr[2]=456;
+        arr[3]=345;
+        System.out.println(Arrays.toString(arr));
+
+    }
+}            
+   
